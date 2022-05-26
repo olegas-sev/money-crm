d846af3dee52ce2f40bb311cd1cdff415a1f1e06
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2022-05-26 15:43:40 +0200

    Allow HTTP req's due Fixer's pricing limitations

--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,7 @@ export default new Vuex.Store({
     async fetchCurrency() {
       const key = process.env.VUE_APP_FIXER
       const res = await fetch(
-        `https://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,SEK`
+        `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,SEK`
       )
       return await res.json()
     }
